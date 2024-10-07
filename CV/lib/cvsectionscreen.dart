8a7f35fdccb6ae7eb0ba43f33810017ba190c393
alyssa <alyssaxlandicho@gmail.com>
2024-10-07 17:54:01 +0800

    activity

---
+++ CV/lib/cvsectionscreen.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:cv/education.dart';
+import 'package:cv/experience.dart';
+import 'package:cv/projects.dart';
+import 'package:cv/skills.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CVSectionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //title: Text('CV Sections'),
+        backgroundColor: const Color.fromRGBO(207, 165, 180, 1),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white,), // Back arrow icon
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous screen
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          // The blue background at the top with a fixed height
+          Container(
+            width: double.infinity,
+            height: 150, // Adjust this value to set the desired height
+            color: const Color.fromRGBO(207, 165, 180, 1),
+            padding: EdgeInsets.all(16),
+            child: SafeArea(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'CV Sections',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // The list of CV sections below
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.school, color: Colors.grey),
+                  title: Text('Education', style: GoogleFonts.fredoka(fontSize: 20)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EducationScreen()),
+                    );
+                  },
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.build_outlined, color: Colors.grey),
+                  title: Text('Skills', style: GoogleFonts.fredoka(fontSize: 20)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SkillsScreen()),
+                    );
+                  },
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.paste_outlined, color: Colors.grey),
+                  title: Text('Projects', style: GoogleFonts.fredoka(fontSize: 20)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProjectsScreen()),
+                    );
+                  },
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.local_library_outlined, color: Colors.grey),
+                  title: Text('Experience', style: GoogleFonts.fredoka(fontSize: 20)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ExperienceScreen()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ stateless_widget/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:stateless_widget/basic_screen.dart';
+
+void main() {
+  runApp(const StaticApp());
+}
+
+class StaticApp extends StatelessWidget {
+  const StaticApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BasicScreen(),
+    );
+  }
+}
+
